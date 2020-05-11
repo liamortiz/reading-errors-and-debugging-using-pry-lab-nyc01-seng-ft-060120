1486d963c0c2aca3cea71b0c497371b3e32b175e
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 00:29:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,5 @@
 # don't forget to add: require 'pry'
 
-require 'pry'
-
 def get_user_input
   gets.chomp
 end
@@ -13,12 +11,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 2
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 1
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
 end
 
 def runner
